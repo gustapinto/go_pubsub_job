7b071d2b5841389720f7b429758f75eaa70ff9f6
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-11-11 08:43:15 -0300

    feat: translate diagram to english

--- scripts/diagram.py
+++ scripts/diagram.py
@@ -8,15 +8,15 @@ with d.Diagram(name="Services", filename="services", curvestyle="curved"):
     publisher = pl.Go(label="Publisher", nodeid="publisher")
 
     with d.Cluster(label="Google Cloud Platform"):
-        pubsub = ga.PubSub(label="Pub/Sub Jobs")
-        firestore = gd.Firestore(label="Firestore Job States")
+        pubsub = ga.PubSub(label="Pub/Sub jobs queue")
+        firestore = gd.Firestore(label="Firestore job status store")
 
     consumer = pl.Go(label="Consumer", nodeid="consumer")
 
     publisher \
-        >> d.Edge(forward=True, label='Envia Jobs Para Mensageria') \
+        >> d.Edge(forward=True, label='Send new jobs to Pub/Sub') \
         >> pubsub \
-        >> d.Edge(reverse=True, label='Consome Jobs da Mensageria') \
+        >> d.Edge(reverse=True, label='Execute job from Pub/Sub message') \
         >> consumer \
-        >> d.Edge(forward=True, reverse=True, label='Pesiste Estado dos Jobs') \
+        >> d.Edge(forward=False, reverse=True, label='Save job status into Firestore') \
         >> firestore
